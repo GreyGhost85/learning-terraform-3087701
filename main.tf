2fb35f00bbe9dd870b3f986e44909a54fcdae1c0
Tony Grey <tgrey@vertafore.com>
2024-09-09 12:19:33 -0400

    lb 2

--- main.tf
+++ main.tf
@@ -46,37 +46,40 @@ resource "aws_instance" "blog" {
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = [
-    {
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = {
+    ex-instance = {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  listeners = {
+    ex-http= {
+      port            = 80
+      protocol        = "HTTP"
+
+      forward = {
+        target_group_key = "ex-instance"
       }
     }
-  ]
+  }
 
-  #http_tcp_listeners = [
-  #  {
-  #    port = 80
-  #    protocol = "HTTP"
-  #    target_group_index = 0
-  #  }
-  #]
-  
+
   tags = {
     Environment = "dev"
-  }
+      }
 }
 
 module "blog_sg" {
