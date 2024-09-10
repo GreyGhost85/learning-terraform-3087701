13934561382aff8063292195adbca9a8fd9babbc
Tony Grey <tgrey@vertafore.com>
2024-09-10 15:52:30 -0400

    formatting fixed

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -30,38 +29,25 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
-
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.0"
-  
+  version = "6.5.2"
+
   name = "blog"
-  min_size = 1
-  max_size = 2
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  #target_group_arns = aws_instance.blog.id
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
-
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -69,29 +55,25 @@ module "blog_alb" {
 
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  vpc_security_group_ids = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
-  }
+  ]
 
-  listeners = {
-    ex-http= {
+
+  http_tcp_listeners = [
+    {
       port            = 80
       protocol        = "HTTP"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
+      target_group_index = 0
     }
-  }
-
+  ]
 
   tags = {
     Environment = "dev"
