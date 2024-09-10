d8b78df271954a4fe80716bf44a24e77b5a5923e
Tony Grey <tgrey@vertafore.com>
2024-09-10 15:12:05 -0400

    reset

--- main.tf
+++ main.tf
@@ -30,24 +30,20 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
-
-  name = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = [module.blog_alb.target_group_arns]
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  subnet_id = module.blog_vpc.public_subnets[0]
+
+
+  tags = {
+    Name = "Learning Terraform"
+  }
 }
 
-module "blog_alb" {
+module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
 
@@ -57,19 +53,26 @@ module "blog_alb" {
 
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  security_groups = module.blog_sg.security_group_id
 
   target_groups = {
+    ex-instance = {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      target_id        = aws_instance.blog.id
     }
+  }
 
   listeners = {
     ex-http= {
       port            = 80
       protocol        = "HTTP"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
     }
   }
 
